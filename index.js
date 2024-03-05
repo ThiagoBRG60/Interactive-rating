2ea673ba9859581b0ec08f24456c556f1e41b000
Thiago Correa <thiagocorrea200375@gmail.com>
2024-03-05 14:50:41 -0300

    js fixes

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const button = document.getElementById("button");
 const ratingMessage = document.getElementById("rating-message");
 const thanksMessage = document.getElementById("thanks-message");
 
-rating.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    rating.forEach((otheritem,otherindex) => {
-      otheritem.classList.toggle("selected", index === otherindex);
-
-      if (item.classList.contains('selected')) {
-        ratingMessage.innerText = "You selected " + item.innerText + " out of 5"
-      }
-      button.addEventListener('click', () => {
-        if(item.classList.contains('selected')) {
-          thanksMessage.style.zIndex = "1"
-        }else {
-          thanksMessage.style.zIndex = "-1"
-        }
-      })
-    });
+for (let i = 0; i < rating.length; i++) {
+  rating[i].addEventListener("click", () => {
+    if (!rating[i].classList.contains("selected")) {
+      rating.forEach((review) => {
+        review.classList.remove("selected");
+      });
+      rating[i].classList.add("selected");
+    } else {
+      rating[i].classList.toggle("selected");
+    }
   });
-});
+
+  button.addEventListener("click", () => {
+    if (rating[i].classList.contains("selected")) {
+      thanksMessage.style.zIndex = "1";
+      ratingMessage.innerText = `You selected ${rating[i].innerHTML} out of 5`
+    }
+  });
+}
